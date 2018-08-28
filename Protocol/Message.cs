96d239899d7c52d6b120014e93e9e55fd4b53592
Andreas Palmblad <p.blad@hotmail.com>
2018-08-28 15:08:50 +0200

    Adding  protocollibrary

---
+++ Protocol/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocol
+{
+    public enum AllActions
+    {
+        Signup,
+        Login,
+
+    }
+
+    public class Message
+    {
+        public bool Loggedin { get; set; }
+        public AllActions Action { get; set; }
+        public string Text { get; set; }
+        public string UserName { get; set; }
+    }
+}
