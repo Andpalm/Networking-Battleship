58874782edac18e562fbe1badb40b5395e9674e8
Henrietta Köhler <henriettakohler@gmail.com>
2018-08-29 15:11:11 +0200

    databas-connection

---
+++ GameServer/Models/Data/HippoContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace GameServer.Models.Data
+{
+    class HippoContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public HippoContext() : base("Server=.;Database=Hippo;Trusted_Connection=True;")
+        {
+           
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
